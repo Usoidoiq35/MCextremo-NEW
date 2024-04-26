2fb0ab3f640e2bdf587f33243dcbc2f72723b9e5
Usoidoiq35 <167512171+Usoidoiq35@users.noreply.github.com>
2024-04-26 09:29:19 +0200

    Update SpectatorListener.java

--- src/main/java/p/mcextremo/spectator/SpectatorListener.java
+++ src/main/java/p/mcextremo/spectator/SpectatorListener.java
@@ -1,28 +1,27 @@
 package p.mcextremo.spectator;
 
-import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerQuitEvent;
-import p.mcextremo.MCextremo;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class SpectatorListener implements Listener {
 
-    private final MCextremo plugin;
+    private final SpectatorManager spectatorManager;
 
-    public SpectatorListener(MCextremo plugin) {
-        this.plugin = plugin;
+    public SpectatorListener(SpectatorManager spectatorManager) {
+        this.spectatorManager = spectatorManager;
     }
 
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        // Verificar si el jugador que sale estaba en modo espectador y manejar según tu lógica
+    public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        if (player.getGameMode() == GameMode.SPECTATOR) {
-            // Puedes realizar acciones específicas cuando un jugador en modo espectador sale del juego
-            SpectatorManager.exitSpectatorMode(player);
+        // Detect right-click or left-click
+        if (event.getAction().name().contains("RIGHT_CLICK") || event.getAction().name().contains("LEFT_CLICK")) {
+            // Change player's perspective
+            spectatorManager.changePerspective(player);
         }
     }
 }
+
 
